a50bf5e759d720a7389264b99e6c117045e40b5b
PAWAR GUNJAN SINGH <71166527+gunjansinghpawar@users.noreply.github.com>
2024-01-25 17:32:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+  const typed = new Typed("#element", {
+    strings: ["WEBSITE DEVELOPER.", "WEB DESIGNER."],
+    typeSpeed: 100,
+    setInterval: 1,
+    loop: true,
+  });
+  function scrollToTop() {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+});
+  }
